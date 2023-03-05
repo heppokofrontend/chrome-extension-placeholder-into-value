994907984687b41eabecc61f397364996818e0e4
heppokofrontend <heppokofrontend@gmail.com>
2023-03-06 00:25:05 +0900

    First commit

--- src/worker.ts
+++ src/worker.ts
@@ -1,97 +1,24 @@
-const state = {
-  tabId: NaN,
-};
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    type: 'normal',
+    id: 'heppokofrontend.placeholder.into.value',
+    title: 'placeholder into value',
+    contexts: ['editable'],
+  });
+});
 
-const onClickContextMenus = async ({ menuItemId }: chrome.contextMenus.OnClickData) => {
-  const tab = await chrome.tabs.get(state.tabId);
+chrome.contextMenus.onClicked.addListener(async ({ menuItemId }) => {
+  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
 
-  if (tab?.url?.startsWith('http')) {
-    chrome.tabs.sendMessage(state.tabId, { menuItemId }).catch(console.log);
-
-    return true;
+  if (!tab.id) {
+    return;
   }
 
-  return false;
-};
+  const tabId = tab.id;
 
-const setTabId = (tabId: number) => {
-  state.tabId = tabId;
-  chrome.contextMenus.onClicked.removeListener(onClickContextMenus);
-  chrome.contextMenus.onClicked.addListener(onClickContextMenus);
+  if (tab.url?.startsWith('http')) {
+    await chrome.tabs.sendMessage(tabId, { menuItemId }).catch((error) => console.log(error, 1));
+  }
 
   return true;
-};
-chrome.tabs.onUpdated.addListener(setTabId);
-chrome.tabs.onActivated.addListener(({ tabId }) => setTabId(tabId));
-chrome.runtime.onInstalled.addListener(() => {
-  const parentId = chrome.contextMenus.create({
-    id: 'heppokofrontend.image.controler',
-    title: 'Image Controler',
-    contexts: ['all'],
-  });
-
-  [
-    {
-      id: 'zoom',
-      title: '拡大',
-      children: [...new Array(12)].map((_, index) => {
-        const value = `${(index + 1) * 0.25 * 100}%`;
-
-        return {
-          id: value,
-          title: value,
-        };
-      }),
-    },
-    {
-      id: 'rotate',
-      title: '回転',
-      children: [...new Array(9)].map((_, index) => {
-        const value = `${index * 45}deg`;
-
-        return {
-          id: value,
-          title: value,
-        };
-      }),
-    },
-    {
-      id: 'reverse',
-      title: '左右反転',
-    },
-    {
-      id: 'dialog',
-      title: '詳細を表示',
-    },
-    {
-      id: 'reset-menus',
-      title: 'リセット',
-      children: [
-        {
-          id: 'reset',
-          title: 'この画像を元に戻す',
-        },
-        {
-          id: 'reset-all',
-          title: 'すべての画像を元に戻す',
-        },
-      ],
-    },
-  ].forEach(({ id, title, children }) => {
-    chrome.contextMenus.create({
-      id,
-      title,
-      contexts: ['all'],
-      parentId,
-    });
-
-    children?.forEach(({ id: childId, title: childTitle }) => {
-      chrome.contextMenus.create({
-        id: childId,
-        title: childTitle,
-        contexts: ['all'],
-        parentId: id,
-      });
-    });
-  });
 });
